06d63aa9a55f244be362c5414c4f8c3858aa6b6b
Bartek W <bewuwy@gmail.com>
2021-03-09 15:51:12 +0100

    added instructions on how to get sp_dc

--- flask_app.py
+++ flask_app.py
@@ -50,5 +50,19 @@ def activity():
         return redirect("/")
 
 
+@app.route("/instrukcja")
+def instrukcja():
+    return """
+    1. Na komputerze w Chrome wejdź na <a href=https://open.spotify.com target="_blank">https://open.spotify.com</a>
+     i zaloguj się <br>
+    2. kliknij Ctrl+Shift+C <br>
+    3. Otworzy ci sie z prawej strony takie okienko, wybierz zakladke Application <br> <img src=static/ins1.png> <br>
+    4. Kliknij w Cookies, a potem w https://open.spotify.com <br> <img src=static/ins2.png> <br>
+    5. W kolumnie sp_dc kliknij dwa razy value (zaznaczone na zielono) <br> <img src=static/ins3.png> <br>
+    6. Kliknij Ctr+C, masz teraz w schowku swój klucz sp_dc <br>
+    7. Wróć na <a href="/">stronę główną</a> i wklej swój klucz <br>
+    """
+
+
 if __name__ == '__main__':
     app.run()
