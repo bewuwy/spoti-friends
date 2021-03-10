a72d249be45c3cc6fdad6ca2026dead1901e9e4d
Bartek W <bewuwy@gmail.com>
2021-03-10 08:36:10 +0100

    updated sp_dc instructions

--- flask_app.py
+++ flask_app.py
@@ -49,17 +49,18 @@ def activity():
         return redirect("/")
 
 
-@app.route("/instrukcja")
-def instrukcja():
+@app.route("/howto")
+def instructions():
     return """
-    1. Na komputerze w Chrome wejdź na <a href=https://open.spotify.com target="_blank">https://open.spotify.com</a>
-     i zaloguj się <br>
-    2. kliknij Ctrl+Shift+C <br>
-    3. Otworzy sie z prawej strony okienko, wybierz zakladke Application <br> <img src=static/ins1.png> <br>
-    4. Kliknij w Cookies, a potem w https://open.spotify.com <br> <img src=static/ins2.png> <br>
-    5. W kolumnie sp_dc kliknij dwa razy value (zaznaczone na zielono) <br> <img src=static/ins3.png> <br>
-    6. Kliknij Ctr+C, masz teraz w schowku swój klucz sp_dc <br>
-    7. Wróć na <a href="/">stronę główną</a> i wklej swój klucz <br>
+    1. In Chrome (on computer) open <a href=https://open.spotify.com target="_blank">https://open.spotify.com</a>
+     and log in. <br>
+    2. Open the Inspector window by pressing Ctrl+Shift+C. <br>
+    3. In the opened window, click the Application tab. <br> <img src=static/ins1.png> <br>
+    4. Choose Cookies, then https://open.spotify.com <br> <img src=static/ins2.png> <br>
+    5. In the sp_dc row click Value two times (marked in green). <br> <img src=static/ins3.png> <br>
+    6. Copy the sp_dc token using Ctr+C. <br>
+    7. Go back to the <a href="/">main page</a> and paste your token. <br>
+    8. The token will be saved automatically in your browser, you will need to change it once a year. <br>
     """
 
 
