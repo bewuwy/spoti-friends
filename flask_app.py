2cdfdc4a0b999e16e79d50251a4753c2bc06fb98
Bartek W <bewuwy@gmail.com>
2021-03-10 09:20:34 +0100

    moved instructions to howto.html and changed alignment in index.html

--- flask_app.py
+++ flask_app.py
@@ -51,17 +51,7 @@ def activity():
 
 @app.route("/howto")
 def instructions():
-    return """
-    1. In Chrome (on computer) open <a href=https://open.spotify.com target="_blank">https://open.spotify.com</a>
-     and log in. <br>
-    2. Open the Inspector window by pressing Ctrl+Shift+C. <br>
-    3. In the opened window, click the Application tab. <br> <img src=static/ins1.png> <br>
-    4. Choose Cookies, then https://open.spotify.com <br> <img src=static/ins2.png> <br>
-    5. In the sp_dc row click Value two times (marked in green). <br> <img src=static/ins3.png> <br>
-    6. Copy the sp_dc token using Ctr+C. <br>
-    7. Go back to the <a href="/">main page</a> and paste your token. <br>
-    8. The token will be saved automatically in your browser, you will need to change it once a year. <br>
-    """
+    return render_template("howto.html")
 
 
 if __name__ == '__main__':
